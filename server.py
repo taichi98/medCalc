4c63bb68070d6a9e2592f27c0abe82007985dc58
taichi98 <hoangthong012@gmail.com>
2024-11-03 15:24:25 +0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import rpy2.robjects as robjects
+
+app = Flask(__name__)
+
+# Load RDS file in R and expose via Python using rpy2
+robjects.r('''
+  load_standards <- function() {
+    list_standards <- readRDS(file.path("code", "standards.rds"))
+    list_standards
+  }
+''')
+load_standards = robjects.globalenv['load_standards']
+standards = load_standards()
+
+@app.route('/calculate_zscore', methods=['POST'])
+def calculate_zscore():
+    data = request.json
+    age = data['age']
+    sex = data['sex']
+    height = data['height']
+    weight = data['weight']
+
+    # Implement the Z-score calculation using standards data
+    # Assuming standards has columns for age, sex, and the LMS values (lambda, mu, sigma)
+
+    result = {
+        "zlen": zlen,
+        "zwei": zwei,
+        "zbmi": zbmi
+    }
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
