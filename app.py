b86b0a8eca939099b88fcb0f6955164ef3cbfee6
taichi98 <hoangthong012@gmail.com>
2024-11-06 16:15:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,8 +94,8 @@ def zscore_calculator():
         bmi = weight / ((height / 100) ** 2)
         
         # Chuyển đổi giới tính thành dạng số (1 = Nam, 2 = Nữ)
-        sex_value = 1 if sex.lower() == "male" else 2
-        
+        sex_value = 1 if sex.lower() == "male"
+        sex_value = 2 if sex.lower() == "female"
         #age_days = age_to_days(age_months, is_age_in_month=True)
         
         # Tính toán Z-score
