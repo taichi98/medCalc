c83c9b94658dd9db4e4bb79817f1d8df00556576
taichi98 <hoangthong012@gmail.com>
2025-01-09 15:17:42 +0700

    Add files via upload

---
+++ php-app/header.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+    <title><?php echo $title ?? 'MedixTools'; ?></title>
+    <link rel="icon" href="/icon.png" type="image/png">
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/script/script.js" defer></script>
+</head>
